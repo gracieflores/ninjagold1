dfbd2a41259c463d695902ade66856fb9bb45d0f
Your Name <you@example.com>
2019-11-08 12:17:30 -0600

    starter code

---
+++ server/config/routes.js
@@ -0,0 +1,10 @@
+// const mongoose = require('mongoose'),
+//       Quote = mongoose.model('Quote')
+module.exports = function(app){
+    // app.get('/', function (req, res) {
+       
+    //     res.render('index.html')
+       
+    // })
+       // all other routes
+}   
